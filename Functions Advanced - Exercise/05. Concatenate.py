40b1c650f8eb7439a37fce7a9ec57130ee54569e
BorisAtias <107293483+BorisAtias@users.noreply.github.com>
2023-12-06 09:04:31 +0100

    Delete Functions Advanced - Exercise directory

--- Functions Advanced - Exercise/05. Concatenate.py
+++
@@ -1,9 +0,0 @@
-def concatenate(*args, **kwargs):
-    result = ""
-    for string in args:
-        result += string
-    for key, value in kwargs.items():
-        if key in result:
-            result = result.replace(key, value)
-
-    return result
