49a14c1aa008b997ece2f2da99c99f3cc5d4f0a8
BorisAtias <107293483+BorisAtias@users.noreply.github.com>
2023-12-13 20:50:48 +0100

    Delete Multidimensional Lists - Exercise 1 directory

--- Multidimensional Lists - Exercise 1/01. Diagonals.py
+++
@@ -1,7 +0,0 @@
-matrix = [[int(x) for x in input().split(", ")] for i in range(int(input()))]
-
-primary_diagonal = [matrix[row][row] for row in range(len(matrix))]
-secondary_diagonal = [matrix[row][len(matrix) - row - 1] for row in range(len(matrix))]
-
-print(f"Primary diagonal: {', '.join(map(str, primary_diagonal))}. Sum: {sum(primary_diagonal)}")
-print(f"Secondary diagonal: {', '.join(map(str, secondary_diagonal))}. Sum: {sum(secondary_diagonal)}")
--- Multidimensional Lists - Exercise 1/05. Matrix of Palindromes.py
+++
@@ -1,6 +0,0 @@
-rows, cols = map(int, input().split())
-
-matrix = [[chr(97 + row) + chr(97 + row + col) + chr(97 + row) for col in range(cols)] for row in range(rows)]
-
-for row in matrix:
-    print(" ".join(row))
