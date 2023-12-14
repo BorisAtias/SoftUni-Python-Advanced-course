c591c2742809fba89f260467c07fa68da7c3ff4a
BorisAtias <107293483+BorisAtias@users.noreply.github.com>
2023-12-14 21:43:43 +0100

    Delete Multidimensional Lists - Exercise 2 directory

--- Multidimensional Lists - Exercise 2/01. Flatten Lists.py
+++
@@ -1,9 +0,0 @@
-all_lists = input().split("|")
-matrix = []
-
-for sub_list in all_lists[::-1]:
-    curr_el = sub_list.split()
-    for el in curr_el:
-        matrix.append(el)
-
-print(" ".join(map(str, matrix)))
